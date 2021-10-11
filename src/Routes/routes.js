76cfdc94c7258247d12eeed2cbe9729eabb6f90b
RobsonNeves03 <robson.gabriel.neves@gmail.com>
2021-10-11 18:21:06 -0300

    Folders and routes fixes

--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, BrowserRouter, Switch } from "react-router-dom";
-import FrontPage from '../Pages/FrontPage';
+import FrontPage from '../Pages/FrontPage/FrontPage';
 
 const Routes = () => {
 
